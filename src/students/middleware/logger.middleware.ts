80f7b279904553b8b1cff5b79b039ae91367ad19
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-22 23:12:43 +0200

    done

--- src/students/middleware/logger.middleware.ts
+++ src/students/middleware/logger.middleware.ts
@@ -1,10 +1,10 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
-import { NextFunction,Request,Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 @Injectable()
-export class LoggerMiddelware implements NestMiddleware {
-    use(req:Request,res:Response,nest:NextFunction){
-        console.log(`Incoming Request: ${req.method} ${req.url}`);
-        nest();
-    }
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log(`Incoming Request: ${req.method} ${req.url}`);
+    next();
+  }
 }
--- src/students/studentModule.student.ts
+++ src/students/studentModule.student.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module,NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentController } from './StudentController.student';
 import { StudentService } from './StudentService.students';
 import { Student } from './Entities/Student.Entity';
+import { LoggerMiddleware } from "./middleware/logger.middleware";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Student])],
@@ -10,4 +11,9 @@ import { Student } from './Entities/Student.Entity';
   providers: [StudentService],
   exports: [StudentService]
 })
-export class StudentModule {}
+
+export class StudentModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware ).forRoutes(StudentController);
+  }
+}
