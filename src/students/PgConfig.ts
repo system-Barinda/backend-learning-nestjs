d2069afffe3b76e7dd9b7b06e9549c9ba384bdad
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-22 23:25:11 +0200

    done

--- src/students/PgConfig.ts
+++ src/students/PgConfig.ts
@@ -1,8 +1,8 @@
-import { DataSourceOptions } from "typeorm";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-export const PgConfig: DataSourceOptions = {
+export const PgConfig: TypeOrmModuleOptions = {
   type: "postgres",
   url: "postgresql://neondb_owner:npg_9FK5scmLbDYH@ep-falling-waterfall-atsiheyd-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-  synchronize: true, 
-  entities: [__dirname + '/**/*.Entities{.ts,.js}'], 
+  synchronize: true,
+  autoLoadEntities: true,
 };
