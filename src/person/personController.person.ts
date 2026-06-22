2aee426167f96f98ec3c105a2b6d8df4e57d4bd6
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-22 19:58:24 +0200

    don

--- src/person/personController.person.ts
+++ src/person/personController.person.ts
@@ -1,7 +1,8 @@
-import { Controller,Body,Post } from "@nestjs/common";
+import { Controller,Body,Post, Patch, Param } from "@nestjs/common";
 import { PersonService } from "./personService.person";
 import { Person } from "src/entities/user.entity";
 import { InsertDataDto } from "src/Dto/InsertDataDto.Dto";
+import { UpdatePersonDto } from "src/Dto/UpdatePersonDto";
 
 @Controller("person")
 export class PersonController {
@@ -11,6 +12,11 @@ export class PersonController {
     createData(@Body() person:InsertDataDto){
     
         return this.personService.InsertData(person);
+    }
+
+    @Patch(":id")
+    update(@Param('id') id:string, @Body() body:UpdatePersonDto){
+    return this.personService.updateData(+id,body)
     }
 
 }
