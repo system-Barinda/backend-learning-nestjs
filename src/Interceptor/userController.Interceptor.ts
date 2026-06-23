f97d4da7ae8e1d7de04852402715a3109464251d
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-23 20:09:41 +0200

    done

---
+++ src/Interceptor/userController.Interceptor.ts
@@ -0,0 +1,14 @@
+import { Controller,Get,Post,Body,UseInterceptors, } from "@nestjs/common";
+import { UserService } from "./userService.interceptor";
+import { TransformInterceptor } from './transform.interceptor'
+
+@Controller("users")
+@UseInterceptors(TransformInterceptor)
+export class UserController {
+    constructor(private readonly userService:UserService){}
+
+    @Get()
+    getUser(){
+        return this.userService.findAll();
+    }
+}
