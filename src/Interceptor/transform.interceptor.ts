0ca6d294abbaf816170a612b2b2e512dfdbf4e3d
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-23 19:58:37 +0200

    done to implement the interceptor

---
+++ src/Interceptor/transform.interceptor.ts
@@ -0,0 +1,10 @@
+import { Injectable,NestInterceptor,ExecutionContext,CallHandler } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { success } from "zod";
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        return next.handle().pipe().map((data) => ({success:true,data}))
+    }
+}
