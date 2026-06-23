9e3f00c91a77e989c9831b3ca3d64676718b9cca
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-23 20:15:06 +0200

    done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './Interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  await app.listen(3000);
 }
+
 bootstrap();
