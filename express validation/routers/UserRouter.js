72f59d522cbce25e010722890503e1c2965ec267
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-26 12:47:41 +0200

    done

--- express validation/routers/UserRouter.js
+++ express validation/routers/UserRouter.js
@@ -1,6 +1,7 @@
 import { getAllData } from "../controllers/UserController.js";
 import { validationRequest } from "../middleware/validateRequest.js";
 import {getAllDataValidation} from "../utils/getAllDataValidation.js"
+import { createDataValidation } from "../utils/createDataValidation.js";
 
 import express from 'express';
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 
 router.get('/data',getAllData,validationRequest,getAllDataValidation );
+router.post('/data',createDataValidation,validationRequest,getAllDataValidation );
 
 export default router;
