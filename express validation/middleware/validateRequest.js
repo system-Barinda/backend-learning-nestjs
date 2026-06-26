583c23ed365d5856a6a9a6ba252d06330e14c681
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-26 11:34:09 +0200

    done

--- express validation/middleware/validateRequest.js
+++ express validation/middleware/validateRequest.js
@@ -4,8 +4,8 @@ export const validationRequest = (req,res,next) => {
     const errors = validationResult;
     if(!errors.isEmpty()){
         return res.status(400).jsone({
-            success:true,
-            error:errors.arrays()
+            success:false,
+            errors:errors.arrays()
         });
         
     }
--- express validation/routers/UserRouter.js
+++ express validation/routers/UserRouter.js
@@ -1,10 +1,13 @@
 import { getAllData } from "../controllers/UserController.js";
+import { validationRequest } from "../middleware/validateRequest.js";
+import {getAllDataValidation} from "../utils/getAllDataValidation.js"
+
 import express from 'express';
 
 const router = express.Router(); 
 
 
 
-router.get('/data',getAllData);
+router.get('/data',getAllData,validationRequest,getAllDataValidation );
 
 export default router;
