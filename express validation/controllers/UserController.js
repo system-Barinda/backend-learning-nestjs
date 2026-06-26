9fa319a1ef550d3ee4e35ecf14cd5b02a1e0e445
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-26 12:58:37 +0200

    done

--- express validation/controllers/UserController.js
+++ express validation/controllers/UserController.js
@@ -1,49 +1,54 @@
-import { success } from "zod";
 import data from "../data.js";
 
-export const getAllData = async (req, res) => { 
+export const getAllData = async (req, res) => {
   try {
-       const { query :{filter,value}, } = req ;
+    const {
+      query: { filter, value },
+    } = req;
 
-       let result = data;
-       if(filter && value){
-        result = data.filter(item =>  String(item[filter]).includes(value));
-       }
+    let result = data;
 
-       return res.status(200).json({
-        success:true,
-        message:'Data fetched successfull',
-        data:result
-       })
+    if (filter && value) {
+      result = data.filter((item) => {
+        if (!(filter in item)) return false;
 
-
-    
-  }catch (err) {
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server Error"
-        });
-
+        return String(item[filter])
+          .toLowerCase()
+          .includes(value.toLowerCase());
+      });
     }
+
+    return res.status(200).json({
+      success: true,
+      message: "Data fetched successfully",
+      data: result,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
 
+export const createData = async (req, res) => {
+  try {
+    const newUser = {
+      id: Math.max(...data.map((user) => user.id), 0) + 1,
+      ...req.body,
+    };
 
-export const createData = async(req,res) => {
-  try{
-
-    const newUser = req.body;
-     data.push(newUser)
+    data.push(newUser);
 
     return res.status(201).json({
-      success:true,
-      message:'data created successfully',
-      data:newUser
-    })
-  }
-  catch(error){
+      success: true,
+      message: "Data created successfully",
+      data: newUser,
+    });
+  } catch (error) {
     return res.status(500).json({
-      success:false,
-      message:"internal server Error"
-    })
+      success: false,
+      message: "Internal Server Error",
+    });
   }
-}
+};
