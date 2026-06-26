8e8622c5c58b2460fa3a375a864ebb7ba3ad31f2
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-26 11:24:37 +0200

    done

--- express validation/controllers/UserController.js
+++ express validation/controllers/UserController.js
@@ -1,27 +1,28 @@
+import { success } from "zod";
 import data from "../data.js";
-import { query } from "express-validator";
 
-export const getAllData = query("filter").isEmpty().isString(),async (req, res) => { 
-
+export const getAllData = async (req, res) => { 
   try {
        const { query :{filter,value}, } = req ;
-        if(filter && value){
-          return res.send(data.filter((user) => user[filter].includes(value)))
-        }
-        return res.send(user)
 
-    // 4. Send the response back (Crucial step!)
-    res.status(200).json({
-      success: true,
-      message: 'Data fetched successfully',
-      data: allData
-    });
+       let result = data;
+       if(filter && value){
+        result = data.filter(item =>  String(item[filter]).includes(value));
+       }
+
+       return res.status(200).json({
+        success:true,
+        message:'Data fetched successfull',
+        data:result
+       })
+
+
     
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "Server error: could not fetch data",
-      error: err.message
-    });
-  }
+  }catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+
+    }
 };
