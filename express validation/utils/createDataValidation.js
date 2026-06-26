d5629edaaa9f5c75a3063f17ab23e4f6d42f17ff
system sylvere BARINDA <systembarinda@gmail.com>
2026-06-26 12:51:10 +0200

    done

--- express validation/utils/createDataValidation.js
+++ express validation/utils/createDataValidation.js
@@ -1,8 +1,38 @@
 import { body } from "express-validator";
 
 export const createDataValidation = [
-    body("name").notEmpty()
-                .withMessage("Name is required"),,
-    body("age").isInt()
-               .withMessage("Age must be a positive integer")          
-]
+
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 3 })
+    .withMessage("Name must be at least 3 characters long"),
+
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 characters long"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please provide a valid email address"),
+
+  body("phone")
+    .trim()
+    .notEmpty()
+    .withMessage("Phone number is required"),
+
+  body("website")
+    .trim()
+    .notEmpty()
+    .withMessage("Website is required")
+    .isURL()
+    .withMessage("Please provide a valid website URL"),
+
+];
