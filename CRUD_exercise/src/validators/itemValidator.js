0c7a71fd4833c144835faada21deef50deb6750d
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-02 19:28:39 +0200

    making the validation of data

---
+++ CRUD_exercise/src/validators/itemValidator.js
@@ -0,0 +1,46 @@
+const { body, param } = require("express-validator");
+
+const validateCreateItem = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required.")
+    .isLength({ min: 3 })
+    .withMessage("Name must be at least 3 characters."),
+
+  body("price")
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage("Price must be a positive number."),
+];
+
+const validateUpdateItem = [
+  param("id")
+    .notEmpty()
+    .withMessage("Item ID is required."),
+
+  body("name")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Name cannot be empty.")
+    .isLength({ min: 3 })
+    .withMessage("Name must be at least 3 characters."),
+
+  body("price")
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage("Price must be a positive number."),
+];
+
+const validateItemId = [
+  param("id")
+    .notEmpty()
+    .withMessage("Item ID is required."),
+];
+
+module.exports = {
+  validateCreateItem,
+  validateUpdateItem,
+  validateItemId,
+};
