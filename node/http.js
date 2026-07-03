32df1bac70ff95ec2a7f0aa0f42f626c0434e044
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-03 21:33:16 +0200

    done to upload

--- node/http.js
+++ node/http.js
@@ -10,18 +10,41 @@
 // });
 
 
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+// // Log all request headers
+//   console.log('Request Headers:', req.headers);
+
+//   // Get specific headers (case-insensitive)
+//   const userAgent = req.headers['user-agent'];
+//   const acceptLanguage = req.headers['accept-language'];
+
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end(`User-Agent: ${userAgent}\nAccept-Language: ${acceptLanguage}`);
+// });
+
+// server.listen(3000);
+
+
+
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-// Log all request headers
-  console.log('Request Headers:', req.headers);
+  // Parse the URL
+  const parsedUrl = url.parse(req.url, true);
 
-  // Get specific headers (case-insensitive)
-  const userAgent = req.headers['user-agent'];
-  const acceptLanguage = req.headers['accept-language'];
+  // Get different parts of the URL
+  const pathname = parsedUrl.pathname; // The path without query string
+  const query = parsedUrl.query; // The query string as an object
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`User-Agent: ${userAgent}\nAccept-Language: ${acceptLanguage}`);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    pathname,
+    query,
+    fullUrl: req.url
+  }, null, 2));
 });
 
 server.listen(3000);
