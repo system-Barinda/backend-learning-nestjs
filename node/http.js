0a7a6612072464845b5d27093f39a705ce9d0151
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-03 21:36:50 +0200

    done ot update the currently worked

--- node/http.js
+++ node/http.js
@@ -28,22 +28,53 @@
 
 
 
+// const http = require('http');
+// const url = require('url');
+
+// const server = http.createServer((req, res) => {
+//   // Parse the URL
+//   const parsedUrl = url.parse(req.url, true);
+
+//   // Get different parts of the URL
+//   const pathname = parsedUrl.pathname; // The path without query string
+//   const query = parsedUrl.query; // The query string as an object
+
+//   res.writeHead(200, { 'Content-Type': 'application/json' });
+//   res.end(JSON.stringify({
+//     pathname,
+//     query,
+//     fullUrl: req.url
+//   }, null, 2));
+// });
+
+// server.listen(3000);
+
+
+
 const http = require('http');
-const url = require('url');
+const { URL } = require('url');
+const querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
-  // Parse the URL
-  const parsedUrl = url.parse(req.url, true);
+  // Using the newer URL API (Node.js 10+)
+  const baseURL = 'http://' + req.headers.host + '/';   const parsedUrl = new URL(req.url, baseURL);
 
-  // Get different parts of the URL
-  const pathname = parsedUrl.pathname; // The path without query string
-  const query = parsedUrl.query; // The query string as an object
+  // Get query parameters
+  const params = Object.fromEntries(parsedUrl.searchParams);
+
+  // Example of building a query string
+  const queryObj = {
+    name: 'John Doe',
+    age: 30,
+    interests: ['programming', 'music']
+  };
+  const queryStr = querystring.stringify(queryObj);
 
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({
-    pathname,
-    query,
-    fullUrl: req.url
+    path: parsedUrl.pathname,
+    params,
+    exampleQueryString: queryStr
   }, null, 2));
 });
 
