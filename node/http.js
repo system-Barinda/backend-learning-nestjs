013aefc22ece55b4464113bea5126471953eab3b
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-03 21:37:50 +0200

    done

--- node/http.js
+++ node/http.js
@@ -56,13 +56,13 @@ const { URL } = require('url');
 const querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
-  // Using the newer URL API (Node.js 10+)
+ 
   const baseURL = 'http://' + req.headers.host + '/';   const parsedUrl = new URL(req.url, baseURL);
 
-  // Get query parameters
+
   const params = Object.fromEntries(parsedUrl.searchParams);
 
-  // Example of building a query string
+
   const queryObj = {
     name: 'John Doe',
     age: 30,
