5d3843298b544f7f0760f52fcd1a7b21770bd3d4
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-05 22:15:09 +0200

    done

--- node/all/Authentication.js
+++ node/all/Authentication.js
@@ -2,3 +2,15 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(session({
+  secret: '',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: process.env.NODE_ENV === 'production', maxAge: 24 * 60 * 60 * 1000 } 
+}));
