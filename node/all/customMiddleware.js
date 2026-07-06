b9f58c9a21cef81774ea815af87b9adb6cf9fad9
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-06 13:02:00 +0200

    done to update the currently

--- node/all/customMiddleware.js
+++ node/all/customMiddleware.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 function logger(req,res,next){
-    console.log("request received at: " + new Date().toISOString());
+    console.log(`Request URL: ${req.url} Request Type: ${req.method}`);
     next();
 }
 
@@ -13,6 +13,9 @@ app.use(logger);
 app.get('/done',(req,res) => {
  res.send("welecome to done page my name is barinda system sylvere");
 });
+app.get('/done/:id',(req,res) => {
+    res.send(`welecome to done page my name is barinda system sylvere and the id is ${req.params.id}`);
+   });
 
 app.listen(8090, () => {
     console.log("server running on port 8090");
