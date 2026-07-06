90f89e03766960c53c474ca6bdbd23cf8214752b
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-06 16:04:31 +0200

    done to update the currently

--- node/graphql/index.js
+++ node/graphql/index.js
@@ -1,27 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`;
+import { typeDefs } from "./schema";
 
 const resolvers = {
   Query: {
