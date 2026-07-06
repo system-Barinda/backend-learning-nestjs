22c66d6ebdf3a8a639cc49c9427353eecbb89e0f
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-06 13:14:21 +0200

    done

--- node/all/AuthenticationMiddleware.js
+++ node/all/AuthenticationMiddleware.js
@@ -1,37 +1,40 @@
 const express = require('express');
-const { el } = require('zod/v4/locales');
 const app = express();
 
-function logger(req,res,next){
+function logger(req, res, next) {
     console.log(`Request URL: ${req.url} Request Type: ${req.method}`);
     next();
 }
 
-function authenticate(req,res,next){
+function authenticate(req, res, next) {
     const authHeader = req.headers['authorization'];
 
-    if(!authHeader ){
+    if (!authHeader) {
         return res.status(401).send('Unauthorized: No token provided');
     }
 
-    const token = authHeader.split('')[1];
+    const token = authHeader.split(' ')[1];
 
-    if(token === 'barinda'){
-        req.user = { id:123,username:'john'};
+    if (token === 'barinda') {
+        req.user = {
+            id: 123,
+            username: 'john'
+        };
         next();
+    } else {
+        res.status(403).send('Invalid token');
     }
-    else{
-        res.status(403).send('invalid token');
-    }
-
 }
 
+app.use(logger);
 
 app.get('/api/protected', authenticate, (req, res) => {
-  res.json({ message: 'Protected data', user: req.user });
+    res.json({
+        message: 'Protected data',
+        user: req.user
+    });
 });
 
-
 app.listen(8080, () => {
-  console.log('Server running on port 8080');
+    console.log('Server running on port 8080');
 });
