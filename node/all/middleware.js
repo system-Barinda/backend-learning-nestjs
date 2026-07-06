33552354d4c69e0d45aab2a50e7f94f4b5b48ec3
system sylvere BARINDA <systembarinda@gmail.com>
2026-07-06 12:40:53 +0200

    done

---
+++ node/all/middleware.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+
+app.use((req, res, next) => {
+  console.log('Middleware 1: This always runs');
+  next();
+});
+
+
+app.use((req, res, next) => {
+  console.log('Middleware 2: This also always runs');
+  next();
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
